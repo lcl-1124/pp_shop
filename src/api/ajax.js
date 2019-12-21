44ecd2560cc26ea76588a93658610485cc161b70
lcl <lichenle1124@qq.com>
2019-12-21 14:58:51 +0800

    init foodshop

---
+++ src/api/ajax.js
@@ -0,0 +1,35 @@
+/*
+封装axios的ajax请求函数模块
+  向外暴露一个ajax函数，返回的是一个promise
+*/
+import axios from 'axios'
+
+export default function ajax(url,data={},type='GET') {
+  return new Promise(function (resolve,reject) {
+    //1.异步发送ajax请求
+    let promise;  //初始化promise
+    if (type === 'GET') {//GET请求
+      //处理query请求参数
+      let dataStr = ''; //定义初始化query
+      Object.keys(data).forEach(key => {
+        dataStr += `${key}=${data[key]}&`;
+      })
+      //处理最后的&符号
+      if (dataStr !== '') {
+        dataStr =  dataStr.subString(0,dataStr.length-1);
+        //添加到url上
+        url += "?" + dataStr;
+      }
+      promise = axios.get(url)
+    } else {//POST请求
+      promise = axios.post(url,data)
+    }
+    promise.then(response => {
+      //2.如果成功执行成功回调resolve()   保存数据，修改为成功状态，返回一个新的promise
+      resolve(response.data)
+    }).catch(error => {
+      //3.如果失败执行失败回调reject()    保存数据，修改为失败状态，返回一个新的promise
+      reject(error)
+    })
+  })
+}
---
+++ src/components/FooterGuide/FooterGuide.vue
@@ -0,0 +1,69 @@
+<template>
+  <footer class="footer_guide">
+    <a href="javascript:;" class="guide_item" :class="{on: $route.path === '/msite'}" @click="goto('/msite')">
+      <span class="item_icon">
+        <i class="iconfont iconkuaicanwaimai"></i>
+      </span>
+      <span>外卖</span>
+    </a>
+    <a href="javascript:;" class="guide_item" :class="{on: $route.path === '/search'}" @click="goto('/search')">
+      <span class="item_icon">
+        <i class="iconfont iconsoushuo"></i>
+      </span>
+      <span>搜索</span>
+    </a>
+    <a href="javascript:;" class="guide_item" :class="{on: $route.path === '/order'}" @click="goto('/order')">
+      <span class="item_icon">
+        <i class="iconfont icondingdan1"></i>
+      </span>
+      <span>订单</span>
+    </a>
+    <a href="javascript:;" class="guide_item" :class="{on: $route.path === '/profile'}" @click="goto('/profile')">
+      <span class="item_icon">
+        <i class="iconfont icongeren"></i>
+      </span>
+      <span>我的</span>
+    </a>
+  </footer>
+</template>
+
+<script>
+  export default {
+    methods: {
+      goto (path) {
+        this.$router.replace(path)
+      }
+    }
+  }
+</script>
+
+<style lang='stylus' rel='stylesheet/stylus'>
+  @import "../../common/stylus/mixins.styl" 
+  .footer_guide 
+    top-border-1px(#e4e4e4) 
+    position fixed 
+    z-index 100 
+    left 0 
+    right 0 
+    bottom 0 
+    background-color #fff 
+    width 100% 
+    height 50px 
+    display flex 
+    .guide_item 
+      display flex 
+      flex 1 
+      text-align center 
+      flex-direction column 
+      align-items center
+      margin 5px 
+      color #999
+      &.on
+        color #358FCB 
+      span
+        font-size 12px 
+        margin-top 2px 
+        margin-bottom 2px 
+        .iconfont 
+          font-size 22px
+</style>
