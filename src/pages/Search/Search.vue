8ad8666a90352f107cd7207cb1015d5387d1cd57
lcl <lichenle1124@qq.com>
2019-12-26 21:04:55 +0800

    12 commit

--- src/pages/Search/Search.vue
+++ src/pages/Search/Search.vue
@@ -91,10 +91,10 @@
         &.search_submit
           float right
           width 18%
-          border 4px solid #02a774
+          border 4px solid #358FCB
           font-size 16px
           color #fff
-          background-color #02a774
+          background-color #358FCB
 
     .list
       .list_container
